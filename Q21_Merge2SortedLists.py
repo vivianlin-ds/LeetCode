2d8cf137a3cdb3db03eb07635fbcb971dbcddf8e
Vivian <vivianlin9887@gmail.com>
2022-01-06 21:48:38 -0800

    Q21 Merge two sorted linked list (WIP)

--- Q21_Merge2SortedLists.py
+++ Q21_Merge2SortedLists.py
@@ -1,20 +1,41 @@
-def mergesimplelists(list1, list2):
-    """
-    Function is for merging two lists.
-    """
-    mergelist = list1 + list2
-    mergelist.sort()
-    return mergelist
+# Need to define ListNode as a class since LeetCode question referred to the object type
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-def mergeTwoLists(list1:, list2):
-    """
-    For LeetCode question, linked lists
-    """
+def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
+    # Use dummy node to store
+    dummy = ListNode(0)
 
+    # Current pointer in the linked list
+    current = dummy
+
+    # Empty list cases
+    if list1 == None:
+        current.next = list2
+        return dummy.next
+    elif list2 == None:
+        current.next = list1
+        return dummy.next
+
+    # Making the merged list
+    else:
+        if list1.val <= list2.val:
+            # Add the number in list1 to current
+            current.next = list1
+            # Move list1 point to next node
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        # Add any leftover nodes in list after above if else statements
+        current.next = list1 or list2
+    return dummy.next
 
 
 def main():
-    print(mergeTwoLists(list1=[1, 2, 3], list2=[1, 3, 4]))
+    print(mergeTwoLists(list1=[1, 2, 4], list2=[1, 3, 4]))
 
 
 if __name__ == '__main__':
