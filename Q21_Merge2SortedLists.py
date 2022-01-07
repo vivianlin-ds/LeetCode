eb975c8afc877fe1bc31cee4a8e5cf055c1e0fe9
Vivian <vivianlin9887@gmail.com>
2022-01-06 20:32:54 -0800

    Q20 Valid Parentheses Code (Incomplete)

---
+++ Q21_Merge2SortedLists.py
@@ -0,0 +1,23 @@
+def mergesimplelists(list1, list2):
+    """
+    Function is for merging two lists.
+    """
+    mergelist = list1 + list2
+    mergelist.sort()
+    return mergelist
+
+def mergeTwoLists(list1:, list2):
+    """
+    For LeetCode question, linked lists
+    """
+
+
+
+
+
+def main():
+    print(mergeTwoLists(list1=[1, 2, 3], list2=[1, 3, 4]))
+
+
+if __name__ == '__main__':
+    main()
