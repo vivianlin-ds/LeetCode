b8c9d3cdf3fc34c3ea0e14e821fd05cc471a43e3
Vivian <vivianlin9887@gmail.com>
2022-01-07 14:29:16 -0800

    Add annotations

--- Q1_TwoSum.py
+++ Q1_TwoSum.py
@@ -1,10 +1,19 @@
 def twoSum(self, nums: list[int], target: int) -> list[int]:
+    # Question: Given an array of integers nums and an integer target,
+    # return indices of the two numbers such that they add up to target.
+
+    # Need to evaluate value and index, use dictionary
     hold = {}
+
     for i, values in enumerate(nums):
         remain = target - values
+
         if remain in hold:
+            # Need to return the index of the numbers
             return [hold[remain], i]
+
         else:
+            # Store 'values' as key and index 'i' as values
             hold[values] = i
 
 
