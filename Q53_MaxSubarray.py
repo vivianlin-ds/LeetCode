ef55b00b321254f2d4cddb089a4871312bdee7d2
Vivian <vivianlin9887@gmail.com>
2022-03-21 12:34:26 -0700

    Q53 finding maximum sum of subarray code

--- Q53_MaxSubarray.py
+++ Q53_MaxSubarray.py
@@ -2,28 +2,21 @@
 # (containing at least one number) which has the largest sum and return its sum.
 # A subarray is a contiguous part of an array.
 
-def maxSubArray(self, nums: list[int]) -> int:
-    # Look at all the possible arrays in the list
-
-    max_sum = 0
-
-    for leftbound in range(0, len(nums)):
-        current_sum = 0
-        for rightbound in range(leftbound, len(nums)):
-            current_sum += nums[rightbound]
-            max_sum = max(current_sum, max_sum)
+def maxSubArray(nums: list[int]) -> int:
+    current_sum = max_sum = nums[0]
+    for number in nums[1:]:
+        # Determine if next number is bigger than previous current_sum
+        current_sum = max(number, current_sum + number)
+        # Determine if max_sum needs to be updated
+        max_sum = max(max_sum, current_sum)
     return max_sum
-
-def linearMaxSubarray(nums: list[int]) -> int:
-
-# Follow-up: If you have figured out the O(n) solution,
-# try coding another solution using the divide and conquer approach, which is more subtle.
 
 
 def main():
-    print(maxSubArray(self=True, nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4]))
-    print(maxSubArray(self=True, nums=[1]))
-    print(maxSubArray(self=True, nums=[5, 4, -1, 7, 8]))
+    print(maxSubArray(nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    print(maxSubArray(nums=[-2, -1]))
+    print(maxSubArray(nums=[5, 4, -1, 7, 8]))
+    print(maxSubArray(nums=[-1, 0, -2]))
 
 
 if __name__ == '__main__':
