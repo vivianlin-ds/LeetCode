322c03744baa10cd28d242c085e481c01a279a77
vivianlin <vivianlin9887@gmail.com>
2022-08-18 17:18:22 -0700

    Q118 solution - updated

--- Q118_PascalsTriangle.py
+++ Q118_PascalsTriangle.py
@@ -2,16 +2,16 @@
 
 def generate(numRows):
     # Create the array for the result.
-    res = [[1]]
+    res = []
     # Add in the designated number of rows.
-    for rows in range(1, numRows):
-        # Each row starts with 1.
-        row = [1]
-        # Add in numbers in between by adding numbers from previous row.
-        for numbers in range(1, rows):
-            row.append(res[rows-1][numbers-1] + res[rows-1][numbers])
-        # Each row ends with 1.
-        row.append(1)
+    for rows in range(numRows):
+        row = []
+        # Add in Pascal's numbers for each row.
+        for numbers in range(rows + 1):
+            if numbers == rows or numbers == 0:
+                row.append(1)
+            else:
+                row.append(res[rows-1][numbers-1] + res[rows-1][numbers])
         res.append(row)
     return res
 
